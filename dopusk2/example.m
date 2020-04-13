23b0201100f556783b7f76f106a513ed0e1fdd46
Alex <caramboox@gmail.com>
2020-04-14 00:46:22 +0300

    rewrited example.m

--- dopusk2/example.m
+++ dopusk2/example.m
@@ -2,7 +2,6 @@ clear all
 close all
 
 m = 8;
-% p = [1, 0, 1, 1];
 p = [1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1];
 snr_val = -5:2:3;
 N = 50000;
@@ -14,7 +13,6 @@ nfig = 1;
 
 figure(nfig);
 nfig = nfig + 1;
-% plot(p_e_mean_arr);
 soft = [0.088383333333332,0.022741666666667,0.001930000000000,2.833333333333333e-05,0];
 q = erfc(sqrt(2 * 10 .^ (snr_val / 10)) ./ sqrt(2)) * 0.5;
 semilogy(snr_val, soft, snr_val, p_e_mean_arr,  'LineWidth', 2);
@@ -22,25 +20,14 @@ legend('мягкое декодирование', 'жесткое декодир
 title('CRC-16');
 xlabel('E_{b}/N_{0}');
 ylabel('P_{bit}');
-% semilogy(snr_val, , 'LineWidth', 2);
 
-% [T] = theor(m, N, 36, tries);
-% [p_ed, p_ed_as, p_ed_super] = theor_ped(c_mes_control, m, q);
 
-% wh = [1.100000000000000e-05,8.000000000000000e-06,4.000000000000000e-06];
-% h = [1.600000000000000e-05,1.200000000000000e-05,0.700000000000000e-05];
-% figure(nfig);
-% nfig = nfig + 1;
-% semilogy(snr_val, wh, snr_val, h);
-% legend('с кодированием', 'без кодирования');
-% title('CRC-16');
-% xlabel('E_{b}/N_{0}');
-% ylabel('P_{ED}');
+[T] = theor(m, N, 36, tries);
 
-% figure(nfig);
-% nfig = nfig + 1;
-% semilogy(snr_val, T, snr_val, (m / 36) * ones(1, size(T, 2)));
-% legend('с шумом', 'без шума');
-% title('CRC-16');
-% xlabel('E_{b}/N_{0}');
-% ylabel('T');
+figure(nfig);
+nfig = nfig + 1;
+semilogy(snr_val, T, snr_val, (m / 36) * ones(1, size(T, 2)));
+legend('с шумом', 'без шума');
+title('CRC-16');
+xlabel('E_{b}/N_{0}');
+ylabel('T');
